08343e1d780b9eea6e03d2b4edb1929dd3bbd62f
higg <higginshiggins2@protonmail.com>
2022-09-12 02:07:57 -0700

    everything working except fileName & unwanted save prompt on page load

---
+++ app.js
@@ -0,0 +1,78 @@
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+var makeJson = function () {
+    "use strict";
+    var date = new Date();
+    var entryArray = Array.from(document.getElementsByClassName("entry")).map(function (elem) {
+        console.log(elem);
+        return elem.innerText;
+    })
+    download(date.getMonth() + date.getFullYear() + ".json", JSON.stringify(entryArray)); 
+    // date.getDate() + 
+}
+
+
+
+var main = function () { 
+    "use strict";
+
+    //tried to make an abstraction and got errors:
+        // keyTrigger(); >> undefined
+        // keyTrigger(postComment()); >> Uncaught ReferenceError: postComment is not defined at <anonymous>:1:1
+    // var keyTrigger = function(arg) {
+    //     $(".comment-input input").on("keydown", function (event) {
+    //         if (event.keyCode == 13) {
+    //             arg;
+    //         };
+    //     });
+    // };
+
+    var postComment = function() {
+        var $newComment = $("<p>")
+        var $usrInput = $(".comment-input input").val();
+        var $wipeInput = $(".comment-input input").val("");
+        var date = new Date();
+
+        if ($usrInput !== "") {
+            $newComment.hide();
+            $newComment.addClass("entry")
+            $newComment.text($usrInput + ", " + date); 
+            $(".comments").append($newComment);
+            $newComment.fadeIn();
+            $wipeInput;
+        }
+
+        if ($usrInput == "") {
+            //do nothing
+        }
+    };
+
+    $(".button").on("click", makeJson);
+
+
+    // keyTrigger(makeJson());
+
+    $(".comment-input input").on("keydown", function (event) {
+        if (event.keyCode == 13) {
+            makeJson();
+        };
+    });
+
+    // keyTrigger(postComment());
+
+    $(".comment-input input").on("keydown", function (event) {
+        if (event.keyCode == 13) {
+            postComment();
+        };
+    });
+};
+
+$(document).ready(main); 
